fc836013e33f4c5197f94b6acb96fb46cdbde323
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2024-10-12 11:53:12 -0500

    Update wezterm.lua

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -1,10 +1,22 @@
-local wezterm = require 'wezterm'
-local act = wezterm.action
-local config = {}
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
 
-config.font = wezterm.font 'JetBrains Mono'
-font_size = 15
+-- This will hold the configuration.
+local config = wezterm.config_builder()
 
+-- This is where you actually apply your config choices
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 25
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 10
+
+-- This allows mouse click copy & paste 
 config.mouse_bindings = {
 	{
 		event = { Down = { streak = 1, button = "Right" } },
